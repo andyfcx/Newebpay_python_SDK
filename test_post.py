66820cb040e0c05d06afc8578704274bf1a0f5e2
andyhuang <andyhuang@mirrormedia.mg>
2021-04-09 22:30:58 +0800

    Use version 1.2

--- test_post.py
+++ test_post.py
@@ -2,16 +2,17 @@ import os
 import requests
 from services import generateEncryptedData
 
-test_url = "https://ccore.newebpay.com/MPG/mpg_gateway"
+test_url = "https://core.newebpay.com/MPG/mpg_gateway"
 
 data = generateEncryptedData()
+
 
 MerchantID = os.getenv('MerchantID')
 session = requests.Session()
 r = session.post(test_url, data={"MerchantID": MerchantID,
                                   "TradeInfo": data['TradeInfo'],
                                  "TradeSha": data['TradeSha'],
-                                 "Version":"1.6"
+                                 "Version":"1.2"
                                   }) #MPG03007
 
 
